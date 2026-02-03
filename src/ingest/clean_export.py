957c874c844691bf507cf570f28ccb05477acc5f
Trang Nguyen <tranguyeenn2007@gmail.com>
2026-02-02 19:39:44 -0500

    why

--- src/ingest/clean_export.py
+++ src/ingest/clean_export.py
@@ -11,43 +11,66 @@ def clean_export(path):
 
     #clean columns
     df.columns = (df.columns.str.strip().str.lower().str.replace(" ", "_"))
+    df = df.drop_duplicates()
 
     #clean author
-    df["author"] = (df["author"].str.strip().str.title())
+    if "title" in df.columns:
+        df["author"] = (df["author"].str.strip().str.title())
 
     #clean my_rating
-    df["my_rating"] = pd.to_numeric(df["my_rating"], errors="coerce")
+    if "my_rating" in df.columns:
+       df["my_rating"] = pd.to_numeric(df["my_rating"], errors="coerce")
 
     #clean avg_rating
-    df["average_rating"] = pd.to_numeric(df["average_rating"], errors="coerce")
+    if "average_rating" in df.columns:
+        df["average_rating"] = pd.to_numeric(df["average_rating"], errors="coerce")
 
     #clean publisher
-    df["publisher"] = (df["publisher"].str.strip().str.title())
+    if "publisher" in df.columns:
+        df["publisher"] = (df["publisher"].str.strip().str.title())
 
     #clean binding
-    df["binding"] = (df["binding"].str.strip().str.title())
+    if "binding" in df.columns:
+        df["binding"] = (df["binding"].str.strip().str.title())
 
     #clean year_published
-    df["year_published"] = (pd.to_numeric(df["year_published"], errors="coerce").astype("Int64"))
+    if "year_published" in df.columns:
+        df["year_published"] = (pd.to_numeric(df["year_published"], errors="coerce").astype("Int64"))
 
     #clean original_pub_yr
-    df["original_publication_year"] = (pd.to_numeric(df["original_publication_year"], errors="coerce").astype("Int64"))
+    if "original_publication_year" in df.columns:
+        df["original_publication_year"] = (pd.to_numeric(df["original_publication_year"], errors="coerce").astype("Int64"))
 
     #clean date_read
-    df["date_read"] = pd.to_datetime(df["date_read"], errors="coerce")
+    if "date_read" in df.columns:
+        df["date_read"] = pd.to_datetime(df["date_read"], errors="coerce")
 
     #clean date_added
-    df["date_added"] = pd.to_datetime(df["date_added"], errors="coerce")
+    if "date_added" in df.columns:
+        df["date_added"] = pd.to_datetime(df["date_added"], errors="coerce")
+
+    #checking date_read and date_added logic
+    if {'date_read', 'date_added'}.issubset(df.columns):
+        df.loc[df['date_read'] < df['date_added'], 'date_read'] = pd.NA
     
     #clean shelves
-    df["shelves"] = (df["shelves"].str.strip().str.title())
+    if "shelves" in df.columns:
+        df["shelves"] = (df["shelves"].str.strip().str.title())
 
     #clean bookshelves
-    df["bookshelves"] = (df["bookshelves"].str.strip().str.title())
+    if "bookshelves" in df.columns:
+        df["bookshelves"] = (df["bookshelves"].str.strip().str.title())
+
+    #clean isbn
+    if "isbn" in df.columns:
+        df["isbn"] = df["isbn"].astype(str).str.replace(r"[^0-9X]", "", regex=True)
+
+    #language normalization
+    if "language_code" in df.columns:
+        df["language_code"] = df["language_code"].str.lower()
 
     #clean review
-    df = df.drop(columns=["my_review"], errors="ignore")
+    if "my_review" in df.columns:
+        df = df.drop(columns=["my_review"], errors="ignore")
     
     return df
-
-clean_export(PATH)
